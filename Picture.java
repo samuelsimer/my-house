5e9539975a92e4495aefd04712745c11c792f96d
samuelremja <samuelremja@gmail.com>
2018-10-17 13:24:24 +0200

    he añadido un atardecer en el que le sol baja

--- Picture.java
+++ Picture.java
@@ -1,68 +1,76 @@
-/**
- * This class represents a simple picture. You can draw the picture using
- * the draw method. But wait, there's more: being an electronic picture, it
- * can be changed. You can set it to black-and-white display and back to
- * colors (only after it's been drawn, of course).
- *
- * This class was written as an early example for teaching Java with BlueJ.
- * 
- * @author  Michael Kolling and David J. Barnes
- * @version 2011.07.31
- */
-public class Picture
-{
-    private Square wall;
-    private Square ces;
-    private Square window;
-    private Triangle roof;
-    private Circle sun;
-
     /**
-     * Constructor for objects of class Picture
+     * This class represents a simple picture. You can draw the picture using
+     * the draw method. But wait, there's more: being an electronic picture, it
+     * can be changed. You can set it to black-and-white display and back to
+     * colors (only after it's been drawn, of course).
+     *
+     * This class was written as an early example for teaching Java with BlueJ.
+     * 
+     * @author  Michael Kolling and David J. Barnes
+     * @version 2011.07.31
      */
-    public Picture()
+    public class Picture
     {
-        // nothing to do... instance variables are automatically set to null
+        private Square wall;
+        private Square ces;
+        private Square window;
+        private Triangle roof;
+        private Circle sun;
+    
+        /**
+         * Constructor for objects of class Picture
+         */
+        public Picture()
+        {
+            // nothing to do... instance variables are automatically set to null
+        }
+    
+        /**
+         * Draw this picture.
+         */
+        public void draw()
+        {
+            wall = new Square();
+            wall.moveHorizontal(-140);
+            wall.moveVertical(20);
+            wall.changeSize(120);
+            wall.makeVisible();
+            
+            ces = new Square();
+            ces.changeColor("green");
+            ces.moveHorizontal(-320);
+            ces.moveVertical(130);
+            ces.changeSize(800);
+            ces.makeVisible();
+            
+            window = new Square();
+            window.changeColor("black");
+            window.moveHorizontal(-120);
+            window.moveVertical(40);
+            window.changeSize(40);
+            window.makeVisible();
+    
+            roof = new Triangle();  
+            roof.changeSize(60, 180);
+            roof.moveHorizontal(20);
+            roof.moveVertical(-60);
+            roof.makeVisible();
+    
+            sun = new Circle();
+            sun.changeColor("yellow");
+            sun.moveHorizontal(100);
+            sun.moveVertical(100);
+            sun.changeSize(80);
+            sun.makeVisible();
+            sun.slowMoveVertical(-100);
     }
-
     /**
-     * Draw this picture.
+     * mira como baja el Sol
      */
-    public void draw()
+    public void atardecer()
     {
-        wall = new Square();
-        wall.moveHorizontal(-140);
-        wall.moveVertical(20);
-        wall.changeSize(120);
-        wall.makeVisible();
-        
-        ces = new Square();
-        ces.changeColor("green");
-        ces.moveHorizontal(-320);
-        ces.moveVertical(130);
-        ces.changeSize(800);
-        ces.makeVisible();
-        
-        window = new Square();
-        window.changeColor("black");
-        window.moveHorizontal(-120);
-        window.moveVertical(40);
-        window.changeSize(40);
-        window.makeVisible();
-
-        roof = new Triangle();  
-        roof.changeSize(60, 180);
-        roof.moveHorizontal(20);
-        roof.moveVertical(-60);
-        roof.makeVisible();
-
-        sun = new Circle();
-        sun.changeColor("yellow");
-        sun.moveHorizontal(100);
-        sun.moveVertical(100);
-        sun.changeSize(80);
-        sun.makeVisible();
-        sun.slowMoveVertical(-180);
+            sun.slowMoveVertical(100);
+            sun.makeInvisible();
     }
 
     /**
